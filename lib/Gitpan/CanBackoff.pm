c158a5c6d537e2324813e475f6c42ba28a991bb7
Michael G. Schwern <schwern@pobox.com>
2013-01-02 21:10:55 -0800

    Detabify.

--- lib/Gitpan/CanBackoff.pm
+++ lib/Gitpan/CanBackoff.pm
@@ -10,11 +10,11 @@ method do_with_backoff(Int :$times=6, CodeRef :$code!, CodeRef :$check) {
     $check //= $self->can("default_success_check");
 
     for my $time (1..$times) {
-	my $return = $code->();
-	return $return if $check->($self, $return);
+        my $return = $code->();
+        return $return if $check->($self, $return);
 
-	# .5 1 2 4 8 ...
-	usleep(2**($time-1)/2);
+        # .5 1 2 4 8 ...
+        usleep(2**($time-1)/2);
     }
 
     return;
