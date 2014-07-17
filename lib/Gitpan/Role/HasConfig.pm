4e627cc08431071da0eced64aef504d90ce888d2
Michael G. Schwern <schwern@pobox.com>
2014-07-17 13:21:33 -0700

    Document Gitpan::Role::HasConfig

--- lib/Gitpan/Role/HasConfig.pm
+++ lib/Gitpan/Role/HasConfig.pm
@@ -18,3 +18,44 @@ has config_class =>
   is            => 'ro',
   isa           => 'Str',
   default       => "Gitpan::Config";
+
+
+=head1 NAME
+
+Gitpan::Role::HasConfig - Per object access to the config
+
+=head1 SYNOPSIS
+
+    {
+        package Some::Class;
+        use Mouse;
+        with 'Gitpan::Role::HasConfig';
+    }
+
+    my $obj = Some::Class->new;
+    my $config = $obj->config;
+
+=head1 DESCRIPTION
+
+With this role your object will have access to the Gitpan configuration.
+
+=head2 Accessors
+
+=head3 config
+
+Access to the config object.
+
+Normally there is no need to set the config.
+
+=head3 config_class
+
+What sub-class of L<Gitpan::Config> should be used to instanciate the
+config object.
+
+Defaults to L<Gitpan::Config>.
+
+=head1 SEE ALSO
+
+L<Gitpan::Config>
+
+=cut
