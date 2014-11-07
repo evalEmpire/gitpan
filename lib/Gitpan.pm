77df76c4350dfb80dc9ff16e961acdfd85a9d501
Michael G. Schwern <schwern@pobox.com>
2014-11-07 13:28:26 -0800

    Eliminate a whole bunch of duplication in Gitpan.pm.
    Merged import_from_distnames() and import_from_backpan_dists().

--- lib/Gitpan.pm
+++ lib/Gitpan.pm
@@ -22,9 +22,46 @@ with 'Gitpan::Role::HasBackpanIndex',
 
 
 method import_from_distnames(
-    ArrayRef $names,
-    Int  :$num_workers          //= 2,
-    Bool :$overwrite_repo         = 0,
+    ArrayRef $distnames,
+    Int  :$num_workers,
+    Bool :$overwrite_repo
+) {
+    my $idx = 0;
+    my $iter = sub {
+        return $distnames->[$idx++];
+    };
+
+    return $self->import_from_iterator(
+        iterator        => $iter,
+        num_workers     => $num_workers,
+        overwrite_repo  => $overwrite_repo
+    );
+}
+
+
+method import_from_backpan_dists(
+    DBIx::Class::ResultSet $bp_dists,
+    Int  :$num_workers,
+    Bool :$overwrite_repo
+) {
+    my $iter = sub {
+        my $bp_dist = $bp_dists->next;
+        return unless $bp_dist;
+        return $bp_dist->name;
+    };
+
+    return $self->import_from_iterator(
+        iterator        => $iter,
+        num_workers     => $num_workers,
+        overwrite_repo  => $overwrite_repo
+    );    
+}
+
+
+method import_from_iterator(
+    CodeRef :$iterator!,
+    Int     :$num_workers       //= 2,
+    Bool    :$overwrite_repo      = 0,
 ) {
     my $fork_man = Parallel::ForkManager->new($num_workers);
 
@@ -38,17 +75,17 @@ method import_from_distnames(
     # child will try to make it at the same time
     Gitpan::Dummy->new->backpan_index;
 
-    $self->main_log("Starting import from distribution names at @{[gmtime->iso8601]}");
+    $self->main_log("Starting import at @{[gmtime->iso8601]}");
 
-    for my $name (@$names) {
-        if( $config->skip_dist($name) ) {
-            $self->main_log( "Skipping $name due to config" );
+    while( my $distname = $iterator->() ) {
+        if( $config->skip_dist($distname) ) {
+            $self->main_log( "Skipping $distname due to config" );
             next;
         }
 
         my $pid = $fork_man->start and next;
         $self->import_from_distname(
-            $name,
+            $distname,
             overwrite_repo => $overwrite_repo
         );
         $fork_man->finish;
@@ -56,68 +93,7 @@ method import_from_distnames(
 
     $fork_man->wait_all_children;
 
-    $self->main_log("Import complete");
-
-    return;
-}
-
-method import_from_backpan_dists(
-    DBIx::Class::ResultSet $bp_dists,
-    Int  :$num_workers          //= 2,
-    Bool :$overwrite_repo         = 0
-) {
-    my $fork_man = Parallel::ForkManager->new($num_workers);
-
-    my $config = $self->config;
-
-    # Parse the CPAN author's file in the parent so each child doesn't
-    # have to redo the work.
-    Gitpan::Dummy->new->build_cpan_authors;
-
-    # Make sure the BackPAN index database is created else every
-    # child will try to make it at the same time
-    Gitpan::Dummy->new->backpan_index;
-
-    $self->main_log("Starting import from BackPAN dists at @{[gmtime->iso8601]}");
-
-    while( my $bp_dist = $bp_dists->next ) {
-        my $distname = $bp_dist->name;
-
-        if( $config->skip_dist($distname) ) {
-            $self->main_log( "Skipping $distname due to config" );
-            next;
-        }
-
-        my $dist = Gitpan::Dist->new(
-            # Pass in the name to avoid sending an open sqlite connection
-            # to the child.
-            distname => $distname
-        );
-
-        my $pid = $fork_man->start and next;
-        $self->import_dist($dist, overwrite_repo => $overwrite_repo);
-        $fork_man->finish;
-    }
-
-    $self->main_log("Import complete");
-
-    $fork_man->wait_all_children;
-}
-
-
-method import_dists(
-    ArrayRef :$search_args,
-    ArrayRef :$order_by_args,
-    Int      :$num_workers
-) {
-    my $bp_dists = $self->backpan_index->dists;
-    $bp_dists = $bp_dists->search_rs(@$search_args) if $search_args;
-    $bp_dists->order_by(@$order_by_args)            if $order_by_args;
-
-    $self->import_from_backpan_dists(
-        $bp_dists,
-        num_workers     => $num_workers
-    );
+    $self->main_log("Import complete at @{[gmtime->iso8601]}");
 
     return;
 }
